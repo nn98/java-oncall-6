ddb493394c5c07fbba76fd1c90ec3ae176f03eae
nn98 <jkllhgb@gmail.com>
2026-01-10 00:39:28 +0900

    feat: 도메인 규칙 파싱에 반영

--- src/main/java/oncall/util/MissionError.java
+++ src/main/java/oncall/util/MissionError.java
@@ -3,8 +3,11 @@ package oncall.util;
 public enum MissionError {
 
     ERROR_PREFIX("[ERROR] "),
+    RETRY_INPUT(" 다시 입력해 주세요."),
     INVALID_INPUT("유효하지 않은 입력값입니다."),
-    RETRY_INPUT(" 다시 입력해 주세요.");
+    OUT_OF_SCOPE_EMPLOYEES("근무자는 5명 이상 35명 이하여야 합니다."),
+    DUPLICATED_EMPLOYEE_NICKNAME("근무자 닉네임은 중복되지 않아야 합니다."),
+    OUT_OF_SCOPE_EMPLOYEE_NICKNAME("근무자 닉네임은 최대 5자여야 합니다.");
 
     private final String message;
 
--- src/main/java/oncall/util/OncallParser.java
+++ src/main/java/oncall/util/OncallParser.java
@@ -2,7 +2,9 @@ package oncall.util;
 
 import java.time.Month;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import oncall.domain.CustomDayOfWeek;
 import oncall.domain.Employee;
 
@@ -22,13 +24,51 @@ public class OncallParser {
 
     public List<Employee> parseEmployees(String input) {
         try {
-            String[] tokens = input.split(",");
-            return Arrays.stream(tokens)
-                    .map(String::trim)
-                    .map(Employee::new)
-                    .toList();
+            List<Employee> employees = parseValidEmployees(input);
+            return employees;
         } catch (Exception exception) {
             throw MissionError.INVALID_INPUT.exception();
         }
     }
+
+    private List<Employee> parseValidEmployees(String input) {
+        String[] employees = checkValidEmployeeNumber(input);
+
+        List<Employee> employeeList = parseValidEmployees(employees);
+
+        isDuplicatedNickname(employeeList);
+
+        return employeeList;
+    }
+
+    private String[] checkValidEmployeeNumber(String input) {
+        String[] employees = input.split(",");
+        if (employees.length < 5 || employees.length > 35) {
+            throw MissionError.OUT_OF_SCOPE_EMPLOYEES.exception();
+        }
+        return employees;
+    }
+
+    private List<Employee> parseValidEmployees(String[] employees) {
+        return Arrays.stream(employees)
+                .map(String::trim)
+                .map(this::isValidNicknameOrElseThrow)
+                .map(Employee::new)
+                .toList();
+    }
+
+    private String isValidNicknameOrElseThrow(String nickname) {
+        if (nickname.length() > 5) {
+            throw MissionError.OUT_OF_SCOPE_EMPLOYEE_NICKNAME.exception();
+        }
+        return nickname;
+    }
+
+    private void isDuplicatedNickname(List<Employee> employeeList) {
+        Set<Employee> employeeSet = new HashSet<>(employeeList);
+        if (employeeList.size() != employeeSet.size()) {
+            throw MissionError.DUPLICATED_EMPLOYEE_NICKNAME.exception();
+        }
+    }
+
 }
